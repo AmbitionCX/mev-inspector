c8d38a7b56d20b5a2f8f2a9264d5272fba5d12a6
AmbitionCX <chenxuanamazing@gmail.com>
2023-03-09 00:20:21 +0800

    [update] change to vite

---
+++ database/sandwiches.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS mev.block_summary
+(
+    inclusion_slot UInt32,
+    inclusion_block_root FixedString(66),
+    inclusion_index UInt32,
+    slot UInt32,
+    committee_index UInt32,
+    aggregation_bits String,
+    beacon_block_root FixedString(66),
+    source_epoch UInt32,
+    source_root FixedString(66),
+    target_epoch UInt32,
+    target_root FixedString(66),
+) ENGINE = MergeTree()
+ORDER BY (inclusion_slot, slot, committee_index)
+PRIMARY KEY (inclusion_slot)
+
+-- aggregation_indices 
