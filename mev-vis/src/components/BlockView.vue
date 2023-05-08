e3d6ccb3c3e402ad350d1111109b207cc32f47c7
cxk555 <624272157@qq.com>
2023-05-08 17:08:05 +0800

    xiangsi

--- mev-vis/src/components/BlockView.vue
+++ mev-vis/src/components/BlockView.vue
@@ -1,9 +1,29 @@
 <template>
   <div id="Blockview">
     <div class="panel-header">Block View</div>
+    <div class="label-wrapper">
+      <div class="color-block-wrapper">
+        <div class="color-item red"></div>
+        <span class="color-label">liquidations</span>
+        <div class="break"></div>
+        <div class="color-item green"></div>
+        <span class="color-label">arbitrages</span>
+        <div class="break"></div>
+        <div class="color-item purple"></div>
+        <span class="color-label">sandwich attacker</span>
+        <div class="break"></div>
+        <div class="color-item orange"></div>
+        <span class="color-label">sandwich defender</span>
+        <div class="break"></div>
+        <div class="color-item blue"></div>
+        <span class="color-label">norm</span>
+      </div>
+    </div>
     <div class="container1">
     <div class="block-container">
     <p>BLOCK&nbsp;{{ this.$store.state.current_selected_block }}</p>
+      <p>{{currentIndex}}</p>
+      <canvas ref="canvas" @mousemove="highlightBlock"></canvas>
     </div>
     <button class="arrow-left" @click="moveLeft">
     </button>
@@ -25,6 +45,8 @@ export default {
       blocks: [],
       blockSize: 10, // 小方块边长
       blockMargin: 1, // 小方块间距
+      like:0,
+      datalike:0,
     };
   },
   created() {
@@ -32,8 +54,11 @@ export default {
   },
   mounted() {
     setTimeout(() => {
+      this.canvas = this.$refs.canvas;
+      this.ctx = this.canvas.getContext("2d");
       this.generateBlocks();
       this.drawBlocks();
+      this.drawHistogram();
     }, 500);
   },
 
@@ -55,10 +80,12 @@ export default {
     '$store.state.current_selected_block': function () {
       const svg = d3.select(this.$refs.svg);
       svg.html("");
+      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
       this.blocks = [];
       setTimeout(() => {
         this.generateBlocks();
         this.drawBlocks();
+        this.drawHistogram();
       }, 300);
 
     },
@@ -83,7 +110,6 @@ export default {
 
     },
     '$store.state.recordsand': function () {
-      console.log("sandwich")
       const svg = d3.select(this.$refs.svg);
       svg.html("");
       this.blocks = [];
@@ -93,40 +119,136 @@ export default {
       }, 300);
 
     },
+currentIndex(newIndex, oldIndex) {
+    // 当 currentIndex 值发生变化时执行此函数
+    // newIndex 是新的 currentIndex 值
+    // oldIndex 是旧的 currentIndex 值
+    const svg = d3.select(this.$refs.svg);
+    svg.html("");
+    this.drawBlocks();
+    // 执行一些操作 ...
+  }
 
 
   },
 
   methods: {
+    islike(a,b){
+      if(a!=0&&b!=0){
+        if(Math.abs(a-b)/a < 0.05){
+          // console.log(1-Math.abs(a-b)/a)
+          return 1-Math.abs(a-b)/a
+
+        }
+        else{return 0}
+      }else{return 0}
+    },
     generateBlocks() {
       const txAmount = this.$store.state.current_block_summary[0].tx_amount; // 获取交易数量
+      const PaidFeeArr = this.$store.state.current_tx_summary.map(item => item.paid_fee)
+      const maxPaidFee = Math.max(...PaidFeeArr)
+      const gasUsedArr = this.$store.state.current_tx_summary.map(item => item.gas_used)
+      const maxGasUsed = Math.max(...gasUsedArr)
       for (let i = 0; i < txAmount; i++) {
         const data = this.$store.state.current_tx_summary[i]; // 获取交易数据
-        const sandwiches = this.$store.state.current_sandwiches.find(tx => tx.frontrun_swap_transaction_hash === data.tx_hash || tx.backrun_swap_transaction_hash === data.tx_hash);
+        const frontrun = this.$store.state.current_sandwiches.find(tx => tx.frontrun_swap_transaction_hash === data.tx_hash );
+        const backtrun = this.$store.state.current_sandwiches.find (tx => tx.backrun_swap_transaction_hash === data.tx_hash);
         const liquidations = this.$store.state.current_liquidations.find(tx => tx.transaction_hash === data.tx_hash);
         const arbitrages = this.$store.state.current_arbitrages.find(tx => tx.transaction_hash === data.tx_hash);
-        if (sandwiches) {
+        const sandefend = this.$store.state.current_sandwiches.find(tx => tx.sandwiched_transaction_hash.includes(data.tx_hash) );
+        const relationship = [];
+        if(frontrun){
+          for (let j=0;j<frontrun.sandwiched_transaction_hash.length;j++){relationship.push(frontrun.sandwiched_transaction_hash[j])}
+          relationship.push(frontrun.backrun_swap_transaction_hash)
+        }
+        if(backtrun){
+          for (let j=0;j<backtrun.sandwiched_transaction_hash.length;j++){relationship.push(backtrun.sandwiched_transaction_hash[j])}
+          relationship.push(backtrun.frontrun_swap_transaction_hash)
+
+        }
+        if (sandefend){
+          relationship.push(sandefend.frontrun_swap_transaction_hash)
+          relationship.push(sandefend.backrun_swap_transaction_hash)
+        }
+
+        if (frontrun||backtrun) {
           const lightness = 10
-          const color = `hsl(210, 90%, ${lightness}%)`; // 生成渐变色
-          this.blocks.push({ color, data });
+          const color = 'purple'; // 生成渐变色
+          this.blocks.push({ color, data ,i,relationship});
         } else {
           if (liquidations) {
             const color = 'red';
-            this.blocks.push({ color, data });
+            this.blocks.push({ color, data,i,relationship });
           } else {
             if (arbitrages) {
               const color = 'green';
-              this.blocks.push({ color, data });
+              this.blocks.push({ color, data,i,relationship });
             } else {
-              const lightness = Math.max(20, 90 - (data.paid_fee / 2000000)); // 计算亮度（使用 value 值作为参考）
-              const color = `hsl(210, 90%, ${lightness}%)`; // 生成渐变色
-              this.blocks.push({ color, data });
+              if(sandefend){
+                const color = 'orange';
+                this.blocks.push({ color,data,i,relationship });
+              }else{
+                this.like = 0
+                this.datalike = 0
+                const left = Math.max(0,i-5)
+                const right = Math.min(txAmount,i+5)
+                for(let j = left;j < right;j++){
+                  if(j!=i&&this.$store.state.current_tx_summary[j].to==data.to){
+                    this.like = 1
+                  }
+                  if(j!=i){
+                    this.datalike = Math.max(this.datalike,this.islike(this.$store.state.current_tx_summary[j].gas_used,data.gas_used)*0.8
+                        +this.islike(this.$store.state.current_tx_summary[j].value,data.value)*0.2)
+                  }
+                }
+
+                const lightness = Math.max(30, 90-this.datalike*20-this.like*5 - 10*(data.paid_fee / maxPaidFee)-5*(data.gas_used/maxGasUsed)); // 计算亮度（使用 value 值作为参考）
+                const color = `hsl(210, 90%, ${lightness}%)`; // 生成渐变色
+                this.blocks.push({ color, data,i,relationship });
+
+              }
+
             }
           }
         }
       }
     },
-
+    drawHistogram() {
+      const canvasWidth = this.canvas.width;
+      const canvasHeight = this.canvas.height;
+      const blockWidth = this.canvas.width/this.$store.state.current_block_summary[0].tx_amount;
+      const maxNum = Math.max(...this.blocks.map((block) => block.data.value));
+      const highlightedIndex = this.highlightedIndex;
+      const highlightedColor = this.highlightedColor;
+      this.blocks.forEach((block, index) => {
+        const x = index * blockWidth ;
+        const height = (block.data.value / maxNum) * canvasHeight;
+        const y = canvasHeight - height;
+        if (index === highlightedIndex) {
+          this.ctx.fillStyle = highlightedColor;
+        } else {
+          this.ctx.fillStyle = block.color;
+        }
+        this.ctx.fillRect(x, y, blockWidth, height);
+      });
+    },
+    highlightBlock(e) {
+      const rect = this.canvas.getBoundingClientRect();
+      const mouseX = e.clientX - rect.left;
+      const mouseY = e.clientY - rect.top;
+      const blockWidth = this.canvas.width / this.$store.state.current_block_summary[0].tx_amount;
+      const index = Math.floor(mouseX / blockWidth);
+      if (index >= 0 && index < this.blocks.length) {
+        this.highlightedIndex = index;
+        this.highlightedColor = 'black';
+        this.currentIndex = index;
+        this.$store.commit('set_current_tx', this.$store.state.current_tx_summary[index]);
+        this.drawHistogram();
+      }
+      else{
+        this.currentIndex = -1;
+      }
+    },
     drawBlocks() {
       const svg = d3.select(this.$refs.svg);
       svg.attr('width', (this.blockSize + this.blockMargin) * this.blockCount);
@@ -141,6 +263,9 @@ export default {
         .attr('width', this.blockSize)
         .attr('height', this.blockSize)
         .attr('fill', d => d.color)
+        .attr('height', (d, i) => i === this.currentIndex ? 11 : 10)
+        .attr('width', (d, i) => i === this.currentIndex ? 11 : 10)
+        .attr('fill', (d, i) => i === this.currentIndex ? 'lightblue' : d.color)
         .on('click', (function (event, d) {
           this.$store.commit('set_current_tx', d.data);
         }).bind(this));
@@ -154,6 +279,13 @@ export default {
         .attr('stroke', 'red')
         .attr('stroke-width', 0.8);
       blocks.on('mouseover', function (event, d) {
+                // 将与当前块相同的块的选中状态更新为 true
+        var currentData = d.relationship;
+        blocks.each(function(d) {
+            if (currentData.includes(d.data.tx_hash)) {
+                d.selected = true;
+            }
+        });
         var table = '<table>';
         for (var key in d.data) {
           table += '<tr><th>' + key + '</th><td>:' + d.data[key] + '</td></tr>\n';
@@ -167,12 +299,24 @@ export default {
           .attr('height', 11)
           .attr('width', 11)
           .style('filter', 'brightness(200%)');
+        var selectedBlocks = blocks.filter(function(d) {
+            return d.selected;
+        });
+        selectedBlocks
+          .transition()
+          .attr('height', 11)
+          .attr('width', 11)
+          .style('filter', 'brightness(200%)');
       }).on('mouseout', function (d) {
+        blocks.each(function(d) {
+            d.selected = false;
+        });
+
         d3.select(this)
           .select('title')
-          .remove()
-        d3.select(this)
-          .transition()
+          .remove();
+
+        blocks.transition()
           .attr('height', 10)
           .attr('width', 10)
           .style('filter', 'brightness(100%)');
@@ -217,12 +361,18 @@ export default {
 }
 
 .block-container {
-  background-color: hsl(210, 90%, 80%);
+  /*background-color: hsl(210, 90%, 80%);*/
   padding: 50px;
-  width: 220px;
+  width: 300px;
   display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 }
 
+canvas {
+display: flex;
+}
 .container {
   display: flex;
   justify-content: center;
@@ -314,10 +464,46 @@ rect {
 .arrow-right {
   right: 20%;
 }
+.label-wrapper {
+  position: absolute;
+  top: 20px;
+  left: 10px;
+}
 
-.fa-chevron-left,
-.fa-chevron-right {
-  font-size: 5px;
-  color: #fff;
+.color-item {
+  display: inline-block;
+  width: 20px;
+  height: 20px;
+}
+
+.red {
+  background-color: red;
+}
+.green {
+  background-color: green;
+}
+.purple {
+  background-color: purple;
+}
+.orange {
+  background-color: orange;
+}
+.blue {
+  background-color: blue;
+}
+.color-label {
+  margin-left: 5px;
+  font-weight: bold;
+}
+
+/* 在小块换行 */
+.color-block-wrapper {
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+}
+.break {
+  flex-basis: 100%;
+  height: 0;
 }
 </style>
--- mev-vis/src/components/OverView.vue
+++ mev-vis/src/components/OverView.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="blockchain">
     <div class="panel-header">Overview</div>
-    <div class="arrows">
-      <button class="arrow arrow-left" @click="move(-1)">左</button>
-      <button class="arrow arrow-right" @click="move(1)">右</button>
+    <div class="slider-container">
+      <input type="range" class="slider" min="1" max="5058" @mouseover="showData"  @mouseleave="hideData" v-model="sliderValue" />
+      <div v-if="showInfo" class="slider-info">{{ sliderValue1+ '~' +sliderValue2 }}</div>
     </div>
     <svg ref="svg"></svg>
   </div>
@@ -19,13 +19,17 @@ export default {
 
   data() {
     return {
+      showInfo: false,
+      sliderValue:0,
       currentPage: 0, // 当前页数
       blockSize: 20, // 小方块边长
-      blockMargin: 40, // 小方块间距
-      blockCount: 20, // 小方块数量
+      blockMargin: 2, // 小方块间距
+      blockCount: 60, // 小方块数量
       blocknum: 12930000 - 12914944 + 1,
       blocks: [], // 所有小方块的数据
       mev_amount: [],
+      sliderValue1:12914944,
+      sliderValue2:12914944+60,
     };
   },
 
@@ -33,17 +37,25 @@ export default {
     this.queryMevAmount();
   },
   mounted() {
-    this.generateBlocks();
-    // this.drawBlocks();
+    this.queryMevAmount();
+      setTimeout(() => {
+        this.generateBlocks();
+        this.drawBlocks(0,this.blockCount)
+      }, 300);
   },
 
   methods: {
+    showData() {
+      this.showInfo = true;
+    },
+    hideData() {
+      this.showInfo = false;
+    },
     queryMevAmount() {
       const path = 'http://localhost:7070/get_mev_amount';
       axios
         .get(path)
         .then(result => {
-          console.log(result.data);
           this.mev_amount = result.data;
         })
         .catch(error => {
@@ -54,56 +66,41 @@ export default {
     generateBlocks() {
       this.blocks = []; // Clear the existing blocks
       const i = 0
-      for (let i = 0; i < 20; i++) {
-        const d = i + 12914944 + this.currentPage * this.blockCount
-        this.$store.commit('queryBlockSummarytemp', d);
+      for (let i = 0; i < 5058; i++) {
+        const text = i + 12914944
+        const num = this.mev_amount[i].total
+        const lightness = Math.max(20, 90 - (num * 8)); // 计算亮度（使用 value 值作为参考）
+        const color = `hsl(210, 90%, ${lightness}%)`; // 生成渐变色
+        this.blocks.push({ color, text });
       }
-      this.$watch(
-        () => this.$store.state.temp_block_summary,
-        (newValue, oldValue) => {
-          const num = newValue[0].sandwiches + newValue[0].arbitrages + newValue[0].liquidations
-          const text = newValue[0].block_number
-          const lightness = Math.max(20, 90 - (num * 10)); // 计算亮度（使用 value 值作为参考）
-          const color = `hsl(210, 90%, ${lightness}%)`; // 生成渐变色
-          if (!this.blocks.some(block => block.text === text)) { this.blocks.push({ color, text }); }
-        }
-      );
-      setTimeout(() => {
-        this.blocks.sort((a, b) => a.text - b.text);
-      }, 200);
-      setTimeout(() => {
-        this.drawBlocks();
-      }, 300);
     },
 
-    drawBlocks() {
+    drawBlocks(startIndex, endIndex) {
       const svg = d3.select(this.$refs.svg);
-      const startIndex = 0;
-      const endIndex = Math.min(this.blockCount, this.blocknum - this.currentPage * this.blockCount);
       svg.attr('width', (this.blockSize + this.blockMargin) * this.blockCount);
       svg.attr('height', 100)
-      const lines = svg.selectAll('line')
-        .data(this.blocks.slice(startIndex, endIndex - 1))
-        .enter()
-        .append('line')
-        .attr('x1', (d, i) => (i % this.blockCount) * (this.blockSize + this.blockMargin) + this.blockSize / 2)
-        .attr('y1', (d, i) => Math.floor(i / this.blockCount) * (this.blockSize + this.blockMargin) + this.blockSize / 2)
-        .attr('x2', (d, i) => {
-          if (i % this.blockCount === this.blockCount - 1) {
-            return 0;
-          } else {
-            return (i % this.blockCount + 1) * (this.blockSize + this.blockMargin) + this.blockSize / 2;
-          }
-        })
-        .attr('y2', (d, i) => {
-          if (i % this.blockCount === this.blockCount - 1) {
-            return (Math.floor(i / this.blockCount) + 1) * (this.blockSize + this.blockMargin) + this.blockSize / 2;
-          } else {
-            return Math.floor(i / this.blockCount) * (this.blockSize + this.blockMargin) + this.blockSize / 2;
-          }
-        })
-        .attr('stroke', 'blue')
-        .attr('stroke-width', '1');
+      // const lines = svg.selectAll('line')
+      //   .data(this.blocks.slice(startIndex, endIndex - 1))
+      //   .enter()
+      //   .append('line')
+      //   .attr('x1', (d, i) => (i % this.blockCount) * (this.blockSize + this.blockMargin) + this.blockSize / 2)
+      //   .attr('y1', (d, i) => Math.floor(i / this.blockCount) * (this.blockSize + this.blockMargin) + this.blockSize / 2)
+      //   .attr('x2', (d, i) => {
+      //     if (i % this.blockCount === this.blockCount - 1) {
+      //       return 0;
+      //     } else {
+      //       return (i % this.blockCount + 1) * (this.blockSize + this.blockMargin) + this.blockSize / 2;
+      //     }
+      //   })
+      //   .attr('y2', (d, i) => {
+      //     if (i % this.blockCount === this.blockCount - 1) {
+      //       return (Math.floor(i / this.blockCount) + 1) * (this.blockSize + this.blockMargin) + this.blockSize / 2;
+      //     } else {
+      //       return Math.floor(i / this.blockCount) * (this.blockSize + this.blockMargin) + this.blockSize / 2;
+      //     }
+      //   })
+      //   .attr('stroke', 'blue')
+      //   .attr('stroke-width', '1');
       const blocks = svg.selectAll('rect')
         .data(this.blocks.slice(startIndex, endIndex)) // 根据当前页数筛选数据
         .enter()
@@ -135,6 +132,7 @@ export default {
           .attr('width', 20)
           .style('filter', 'brightness(100%)');
       });
+
       // 添加连线
 
     },
@@ -158,6 +156,18 @@ export default {
       // this.drawBlocks();
     },
   },
+      watch: {
+      sliderValue(newValue) {
+        console.log(newValue)
+        this.clearBlocks();
+        const startIndex = parseInt(newValue);
+        const lastIndex = Math.min(startIndex + this.blockCount - 1,5058);
+        console.log(startIndex,lastIndex)
+        this.drawBlocks(startIndex, lastIndex);
+        this.sliderValue1 = startIndex+12914943
+        this.sliderValue2 = lastIndex+12914943
+    },
+  },
 };
 </script>
 <style>
@@ -166,7 +176,13 @@ export default {
   flex-direction: column;
   align-items: center;
 }
-
+.slider-container {
+  max-width: 1000px;
+  margin: auto;
+}
+.slider {
+  width: 200%; /* 设置滑块的宽度为父容器的宽度 */
+}
 .arrows {
   display: flex;
   margin-top: 20px;
